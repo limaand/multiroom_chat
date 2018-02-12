3ee5242891766fd7c1dd015ce9f22a605ce144da
Anderson Pereira <limaand@gmail.com>
2018-02-12 16:16:46 -0300

    adicionado as validações do form da index

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,19 @@
 module.exports.iniciaChat = function(application,req, res){
+     
+     var dadosForm = req.body;
+     
+     req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+     req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3,15);
+
+     var erros = req.validationErrors();
+     
+     if(erros){
+         //res.send('Existem erros no formulario');
+         res.render("index", {validacao: erros});
+         
+         return;
+     }
+     
+     //console.log(dadosForm);
      res.render('chat');
 }
